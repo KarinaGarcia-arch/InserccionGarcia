63fa3cfa1b5ec8dd623e7237bd5a14bc44b8349a
USUARIO <wgarciaf@est.ups.edu.ec>
2024-10-02 21:17:59 -0500

    Se completo la funcionalidad del prgrama

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner leer = new Scanner(System.in);
+        int tamaño ;
+        System.out.println("De que tamaño desea el arreglo");
+        tamaño= leer.nextInt();
+        int[]arreglo = new int[tamaño];
+
+        for(int i=0;i<tamaño;i++){
+            System.out.println("Ingrese el numero en la posicion "+(i+1));
+            arreglo[i]= leer.nextInt();
+        }
+
+        System.out.println("Desea ordenar con pasos ");
+        System.out.println("1)SI");
+        System.out.println("2)NO");
     }
 }
